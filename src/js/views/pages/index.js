9b9cffd394e8c382969fdf41408ae461c1ad5b71
Berthold, Robert <rberthol@gannett.com>
2014-09-23 11:07:27 -0400

    map to page using 2012's basemap. started exploring border drawing

--- src/js/views/pages/index.js
+++ src/js/views/pages/index.js
@@ -24,6 +24,39 @@ function ($, _, Backbone, ResultList, ResultMap, BalanceChart, dataManager, Inde
         template: _.template(templateFile),
         
         useOembedTemplate: false,
+
+        data: {
+                geo: {
+                    states: {
+                        simp: [],
+                        zoom: [],
+                        centroids: []
+                    },
+                    counties: {
+                        zoom: {}, // lazy load. properties for state fips
+                        simp: []
+                    },
+                    cds: {
+                        zoom: {}, // lazy load. properties for state fips
+                        simp: []
+                    },
+                    places: []
+                },
+
+                results: {
+                    house: {}, // level => state fips => results
+                    senate: {},
+                    governor: {}
+                },
+
+                totals: {
+                    house: {},
+                    senate: {},
+                    governor: {}
+                },
+
+                initiatives: {}
+            },
         
         initialize: function () {
             balanceChart = new BalanceChart();
@@ -50,7 +83,11 @@ function ($, _, Backbone, ResultList, ResultMap, BalanceChart, dataManager, Inde
             this.$("#map").html(resultMap.el);
             this.$('#list').html(resultList.el);
             L.mapbox.accessToken = 'pk.eyJ1IjoidHJlYmxla2lja2VyIiwiYSI6IjRKTXZtUUEifQ.VBdcmyofyon7L2RFAuGsXQ';
-            var map = L.mapbox.map('mapbox', 'treblekicker.jiojdl2a');
+            var map = L.mapbox.map('map', 'usatoday.map-hdtne5p8', {
+                scrollWheelZoom: false,
+                zoomControl: false
+            }).setView([38.00, -98.00], 4);
+            //this.stateBorders();
             
             return this;
         },
@@ -85,7 +122,73 @@ function ($, _, Backbone, ResultList, ResultMap, BalanceChart, dataManager, Inde
             balanceChart.model.updateTime = dataManager.summary.updateTime;
 
             balanceChart.render();
-        }
+        },
+
+        stateBorders: function (){
+                console.log('stateBorders');
+                var features = this.data.geo.states.zoom;
+
+                /*if (IE) {
+                    var set = view.svg.append('set');
+                    set.selectAll('path')
+                        .this.data(features).enter().append('path')
+                            .attr('class', 'states')
+                            .attr('fill', colorize)
+                            .attr('stroke-width', function(d) {
+                                if (d.id === state) return 2;
+                                return 1;
+                            })
+                            .attr("d", path)
+                            .attr('fill-opacity', function(d) {
+                                if (d.id === state) return 0;
+                                if (!state) return 0;
+                                return 0.25;
+                            })
+                            .attr('stroke', '#ffffff')
+                            .on('click', click);
+                    return undefined;
+                }*/
+
+                // If no state layer exists
+                if (!view.svg.shapes.selectAll('path.states')[0].length) {
+
+                    // Add it
+                    view.svg.shapes.selectAll('path.states')
+                        .this.data(features).enter().append('path')
+                            .attr('class', 'states')
+                            .attr('fill', colorize)
+                            .attr('pointer-events', 'painted')
+                            .attr('stroke-width', function(d) {
+                                if (d.id === state) return 2;
+                                return 1;
+                            })
+                            .attr("d", path)
+                            .attr('fill-opacity', function(d) {
+                                if (race !== 'house' && d.id !== state) return 1;
+                                return 0;
+                            });
+                } else {
+                    view.svg.shapes.selectAll('path.states').each(function(d) {
+                            this.parentNode.appendChild(this);
+                    });
+                }
+
+                // Colorize state layer
+                view.svg.shapes.selectAll('path.states')
+                    .attr('stroke', '#ffffff')
+                    .on('touchstart', touch)
+                    .on('click', click)
+                    .transition().ease(ease).duration(timer)
+                    .attr('stroke-width', function(d) {
+                        if (d.id === state) return 2;
+                        return 1;
+                    })
+                    .attr("d", path)
+                    .attr('fill', colorize)
+                    .attr('fill-opacity', 0.25);
+            }
+
+
     });
     
     return indexView;
