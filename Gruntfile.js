e8f489a86fde32fdb24182886ea599377d955582
Josh Trout <jtrout@gannett.com>
2014-09-23 09:50:03 -0400

    Removing second occurance of masonry

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,7 +193,6 @@ module.exports = function (grunt) {
                         bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
                         d3: '../bower_components/d3/d3',
                         'jquery.hashchange': '../bower_components/jquery-hashchange/jquery.ba-hashchange',
-                        masonry: '../bower_components/masonry/dist/masonry.pkgd',
                         moment: '../bower_components/momentjs/moment',
                         respond: '../bower_components/respond/dest/respond.src',
                         typeahead: '../bower_components/typeahead.js/dist/typeahead',
