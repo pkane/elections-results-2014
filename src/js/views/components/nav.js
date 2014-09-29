e136de0815e99d5093ef16f59b62866fc81295f1
Chris Kamsler <chriskamsler@gmail.com>
2014-09-29 12:48:19 -0400

    sharing working now

--- src/js/views/components/nav.js
+++ src/js/views/components/nav.js
@@ -3,9 +3,11 @@ define([
     'underscore',
     'backbone',
     'models/navModel',
-    'text!views/components/nav.html'
+    'models/config',
+    'text!views/components/nav.html',
+    'events/analytics'
 ],
-function ($, _, Backbone, NavModel, templateFile) {
+function ($, _, Backbone, NavModel, config, templateFile, analytics) {
 
     var indexView = Backbone.View.extend({
         
@@ -15,30 +17,88 @@ function ($, _, Backbone, NavModel, templateFile) {
         
         template: _.template(templateFile),
 
+        text: {
+            message: _.template('Learn more about key #election2014 <%- race %> races via the @USATODAY Election Outlook'),
+
+            facebook: _.template('https://www.facebook.com/dialog/feed/?app_id=<%- appid %>&link=#url#&redirect_uri=#url#name=<%- message %>'),
+            twitter: _.template('https://twitter.com/intent/tweet?url=#url#&text=<%- message %>'),
+            mail: _.template('mailto:?subject=<%- message %>&body=#url#')
+        },
+
         events: {
-            "click .elections-bar-nav-item .office-placeholder"             : "openOverlay",
-            "click .close-nav"                                              : "closeOverlay"
-        },        
+            'click .share-icon': 'shareToggle',
+            'click .elections-bar-nav-item .office-placeholder'             : "openOverlay",
+            'click .close-nav'                                              : "close",
+            'click #facebook-share': 'shareFacebook',
+            'click #twitter-share': 'shareTwitter',
+            'click #mail-share': 'shareEmail',
+        },
+
+        _getShareUrl: function() {
+            return escape(location.href);
+        },
+
+        shareFacebook: function(e) {
+            console.log("share facebook ", this._getShareUrl());
+
+            analytics.trigger('track:event', "elections2014facebook");
+
+            if (window.FB) {
+
+               e.preventDefault(); 
+
+               window.FB.ui({
+                  method: 'share_open_graph',
+                  action_type: 'og.likes',
+                  action_properties: JSON.stringify({ object: this._getShareUrl() })
+                }, function(response){});
+                     
+            }
+
+            this.shareToggle();
+        },
+
+        shareTwitter: function(e) {
+            console.log("share twitter ", e);
+
+            analytics.trigger('track:event', "elections2014twitter");
+
+            if (!config.isMobile) {
+                e.preventDefault();
+                window.open(e.target.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=550,height=420');
+            }                
+
+            this.shareToggle();
+        },
+
+        shareEmail: function(e) {
+            console.log('share email');
+            analytics.trigger('track:event', "elections2014email");
+            this.shareToggle();            
+        },
         
+        shareToggle: function () {
+            this.$el.find(".share-overlay").toggle();
+        },
+
         initialize: function () {
-            
-            $('#election-bar-content').html(this.el);
-            
+            $('#election-bar-content').html(this.el);                        
         },  
         
-        render: function () {
-            
+        render: function () {            
             this.$el.html(this.template(this.model));
-
             this.refresh();
-            
+
             return this;
         },
         
         refresh: function () {
             var race = this.model.currentRace,
-                state = this.model.currentState;
-            
+                state = this.model.currentState,
+                message = escape(this.text.message({ race: race.display })),
+                url = this._getShareUrl()
+                ;
+
             this.$('.election-office-projection-heading').text(race.display + ' Results');
             
             this.$('.elections-bar-nav-item').removeClass('active-item');
@@ -46,9 +106,12 @@ function ($, _, Backbone, NavModel, templateFile) {
             this.$('.elections-bar-nav-item-' + race.key).addClass('active-item');
             
             this.$('.page-icon .icon').removeClass().addClass('icon ' + this.model.getStateIcon());
-            
+
+            this.$('#facebook-share').attr('href', this.text.facebook({ appid: config.pageInfo.facebook.app_id, message: message }).replace(/#url#/g, url));
+            this.$('#mail-share').attr('href', this.text.mail({ message: message }).replace(/#url#/g, url));            
+            this.$('#twitter-share').attr('href', this.text.twitter({ message: message }).replace(/#url#/g, url));
         },
-        
+
         toggleOverlay: function(target) {
             var overlay = $('.election-overlay');
             if (overlay.hasClass("clicked")) {
@@ -66,7 +129,7 @@ function ($, _, Backbone, NavModel, templateFile) {
             }
         },   
 
-        closeOverlay: function(e) {
+        close: function(e) {
             var overlay = this.$el.find(".election-overlay");
             overlay.removeClass("clicked");
         },             
