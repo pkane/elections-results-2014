5e45548cf2962948e677561a789df7e5f87a00c3
Josh Trout <jtrout@gannett.com>
2014-10-21 14:48:41 -0400

    Starting to explore caching, refactored tooltip labels

--- src/js/views/components/resultMap.js
+++ src/js/views/components/resultMap.js
@@ -19,10 +19,19 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
         
         template: _.template(resultMap),
 
+        mapCache: { path: 'none', places: 'none' },    
+            
         drawMap: function(geoJsonPath, geoJsonPlaces, strokeWidth) {
+            var isCached = (geoJsonPath === this.mapCache.path && geoJsonPlaces === this.mapCache.places),
+                noRace = !this.model.race;
 
-            if (this.model.race) {
-                console.log('model...', this.model);
+            if (noRace) {
+                console.count('Short circuit');
+            } else {
+                console.count('DRAW ' + geoJsonPath+geoJsonPlaces);
+
+                this.mapCache.path = geoJsonPath;
+                this.mapCache.places = geoJsonPlaces;
 
                 d3.json(geoJsonPath, _.bind(function(json) {
                     this.svg.html('');
@@ -117,17 +126,12 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
                                         if (d.properties.placement === 'right') return 10;
                                         if (d.properties.placement === 'left') return -10;
                                         return 0;
-                                    })
-                                    ;
-
+                                    });
 
                             }, this));
-
                          }                     
 
-                }, this)); 
-
-
+                }, this));
             }
         },
 
@@ -171,34 +175,34 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
             var found = this.findItemById(d.id);
 
             if (found) {
-                var uncontested = false;
-                if (found.results.length === 1) {
-                    uncontested = true;
-                }
-                tooltip                    
-                    .html([
+                var uncontested = (found.results.length === 1),
+                    tooldata = {
+                        heading: d.properties.name + ((this.model.state && (this.model.race.id != 'h')) ? ', ' + this.model.state.display : ''),
+                        pctReporting: (!uncontested ? found.precincts.pct.toFixed(1) + '% Precincts reporting' : '')
+                    },
+                    templateItem = _.template('<tr><td><%= label %><%= win %></td>'
+                        + '<td class="right"><%= votes %></td>'
+                        + '<td class="right" style="text-align:right"><%= pct %></td></tr>'),
+                    templateTooltip = _.template('<h4 class="map-tooltip-heading"><%= heading %></h4>'
+                        + '<table class="table table-condensed"><thead><tr><th>Candidate</th><th class="right">Votes</th><th class="percent">%</th></tr></thead><tbody>'
+                        + '<%= listing %>'
+                        + '</tbody></table>'
+                        + '<span class="muted"><%= pctReporting %></span>');
 
-                        '<h4 class="map-tooltip-heading">', d.properties.name + ((this.model.state && (this.model.race.id != 'h')) ? ', ' + this.model.state.display : ''), '</h4>',
-                        '<table class="table table-condensed"><thead><tr><th>Candidate</th><th class="right">Votes</th><th class="percent">%</th></tr></thead><tbody>',
-
-                        _.chain(found.results).sortBy('seatNumber').reduce(function(memo, item, index, list) {
-                            if (list[index-1] && list[index-1].seatNumber !== item.seatNumber) {
-                                memo += '<tr><td colspan="3"><hr /></td></tr>';
-                            }
-                            return (memo
-
-                                + '<tr><td>' + (item.name ? item.name : 'Other') + (((item.name != '') && item.party) ? ' (' + item.party.substr(0,1).toUpperCase() + ')' : '')
-                                + (item.win ? '<span class="won"></span>' : '')
-
-                                + '</td>'
-                                + '<td class="right">' + (!uncontested ? voteFormat(item.votes) : 'Uncontested') + '</td>'
-                                + '<td class="right" style="text-align:right">' + (!uncontested ? item.pct.toFixed(1) + '%' : '') + '</td></tr>');
-                        }, '').value(),
-
-                        '</tbody></table>',
-                        '<span class="muted">' + (!uncontested ? found.precincts.pct.toFixed(1) + '% Precincts reporting' : '') + '</span>'
-                    ].join(''))
-                    ;
+                tooldata.listing = _.chain(found.results).sortBy('seatNumber').reduce(function(memo, item, index, list) {
+                    var itemData = {
+                        label: (item.name ? item.name : 'Other') + (((item.name != '') && item.party) ? ' (' + item.party.substr(0,1).toUpperCase() + ')' : ''),
+                        win: (item.win ? '<span class="won"></span>' : ''),
+                        votes: (!uncontested ? voteFormat(item.votes) : 'Uncontested'),
+                        pct: (!uncontested ? item.pct.toFixed(1) + '%' : '')
+                    };
+                    if (list[index-1] && list[index-1].seatNumber !== item.seatNumber) {
+                        memo += '<tr><td colspan="3"><hr /></td></tr>';
+                    }
+                    return memo + templateItem(itemData);
+                }, '').value(),
+                
+                tooltip.html(templateTooltip(tooldata));
                 
                 this.offsetTop = this.$el.offset().top;
                 this.mouseMove(d, i);
@@ -232,7 +236,6 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
             tooltip.classed('hidden', true);
         },
         
-
         fillColor: function(d) {
             var id = d.id,
                 partyColors = config.partyColors,
@@ -241,7 +244,6 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
                 found,
                 color = partyColors.default
                 ;
-
 
             if (this.model.race.id == 'h') {
                 state = id.substr(0, 2);
@@ -337,40 +339,35 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
         },
 
         renderMap: function() {
-            console.log('-- render map ', this.model.race, this.model.state);
-
-            var width = this.$el.width(), 
-                height = Math.floor(width * 3 / 4),
-                scaleMultiplier = 1.0;
-                natScaleMultiplier = 1.2;
-                ;
-
-            if ($('#main-map').width() < 402) {
-                scaleMultiplier = 0.6;
-                natScaleMultiplier = 0.65;
-            } else if ($('#main-map').width() < 610) {
-                scaleMultiplier = 0.9;
-                natScaleMultiplier = 0.9;
-            }
-
-            this.svg = d3.select(this.el)
-                         .select('svg')
-                         .attr('width', width)
-                         .attr('height', height)
-                         ;
-
-            this.projection = d3.geo
-                                .albersUsa()
-                                .translate([width/2, height/2])
-                                .scale([800 * natScaleMultiplier])
-                                ;
-
             if (this.model.race) {
-                if (this.model.state) {                        
+
+                var width = this.$el.width(), 
+                    height = Math.floor(width * 3 / 4),
+                    scaleMultiplier = 1.0,
+                    natScaleMultiplier = 1.2;
+
+                if ($('#main-map').width() < 402) {
+                    scaleMultiplier = 0.6;
+                    natScaleMultiplier = 0.65;
+                } else if ($('#main-map').width() < 610) {
+                    scaleMultiplier = 0.9;
+                    natScaleMultiplier = 0.9;
+                }
+
+                this.svg = d3.select(this.el)
+                             .select('svg')
+                             .attr('width', width)
+                             .attr('height', height)
+                             ;
+
+                if (this.model.state) {
+                    console.count('-- render STATE map');
+
                     d3.json(dataManager.getGeo('states', 'centroids'), _.bind(function(json) {
 
                         var found = _.findWhere(json.features, { id: this.model.state.id });                            
 
+                        console.log('Setting projection', found.geometry.scale * scaleMultiplier);
                         this.projection = d3.geo
                                             .mercator()
                                             .translate([width/2, height/2])
@@ -387,7 +384,15 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
                     }, this));
 
                 } else {
+                    console.count('-- render NATIONAL map');
 
+                    console.log('Setting projection', 800 * natScaleMultiplier); 
+                    this.projection = d3.geo
+                                        .albersUsa()
+                                        .translate([width/2, height/2])
+                                        .scale(800 * natScaleMultiplier)
+                                        ;
+                    
                     if (this.model.race.id == 'h') {
                         this.drawMap(dataManager.getGeo('cds', 'simp'), '', 0.3);
                     } else {
@@ -396,10 +401,8 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
 
                 }
             }
-
         }
 
-        
     });
     
     return view;
