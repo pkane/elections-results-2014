ad4c2616079f3b1331233b6d5b3c6bce58694c15
Berthold, Robert <rberthol@gannett.com>
2014-10-21 13:40:56 -0400

    pull a fillColor console.log

--- src/js/views/components/resultMap.js
+++ src/js/views/components/resultMap.js
@@ -234,7 +234,6 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
         
 
         fillColor: function(d) {
-            console.log('fillColor');
             var id = d.id,
                 partyColors = config.partyColors,
                 state = id,
