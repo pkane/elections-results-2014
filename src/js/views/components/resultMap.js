ceabca67d65a57c4846f1f960eb2773fde92f979
Berthold, Robert <rberthol@gannett.com>
2014-10-31 17:22:00 -0400

    remove comment

--- src/js/views/components/resultMap.js
+++ src/js/views/components/resultMap.js
@@ -215,7 +215,6 @@ function ($, _, Backbone, config, dataManager, fipsMap, resultMap, D3, analytics
 
             if (found) {
                 var uncontested = (found.results && found.results.length === 1),
-                    //heading = d.properties.name + ((this.model.state && (this.model.race.id != 'h')) ? (d.properties.name ? ', ' : '') + this.model.state.display : ''),
                     tooldata = {
                         heading: d.properties.name + ((this.model.state && (this.model.race.id != 'h')) ? (d.properties.name ? ', ' : '') + this.model.state.display : ''),
                         pctReporting: (!uncontested ? found.precincts.pct.toFixed(1) + '% Precincts reporting' : ''),
